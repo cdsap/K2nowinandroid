fd0779f7359c9d0cf58cbab44aa4de708c078a6c
Inaki Villar <inaki.seri@gmail.com>
2023-07-31 15:56:24 -0700

    a

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -45,7 +45,7 @@ buildCache {
     remote<HttpBuildCache> {
         isAllowUntrustedServer = true
         isAllowInsecureProtocol  = true
-        url = uri("http://34.100.208.149:5071/cache/")
+        url = uri("http://34.151.73.33:5071/cache/")
         isEnabled = true
         isPush = System.getenv("CI") != null
 
